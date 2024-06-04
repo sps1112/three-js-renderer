fde020ed5413e9c96840384f2f19d6b622dd8015
Siddhartha Pratap Singh <sidps1112@gmail.com>
2024-06-04 15:28:38 +0530

    Update to vite and three modoules

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
-// Load the Canvas
-const canvas = document.querySelector(".renderer");
+import * as THREE from "three";
 
 // Define the Viewport
 const sizes = {
@@ -7,40 +6,59 @@ const sizes = {
   height: 600,
 };
 
-// Create the Scene
-const scene = new THREE.Scene();
+// Load the Canvas
+const canvas = document.getElementById("sceneCanvas");
+canvas.width = sizes.width;
+canvas.height = sizes.height;
 
 // Define the Renderer
-console.log(canvas);
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
 });
-renderer.setSize(sizes.width, sizes.height);
+renderer.setSize(canvas.width, canvas.height);
+
+// Create the Scene
+const scene = new THREE.Scene();
 
 // Define the Camera
 const camera = new THREE.PerspectiveCamera(
-  75,
-  sizes.width / sizes.height,
+  60,
+  canvas.width / canvas.height,
   0.1,
   1000
 );
-camera.position.z = 3;
-camera.position.x = -1;
+camera.position.z = 5;
+camera.position.x = 0;
 scene.add(camera);
 
-// Define the Cube
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const cube = new THREE.Mesh(geometry, material);
+// Define basic geometry
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+const sphereGeometry = new THREE.SphereGeometry(1, 18, 18);
+
+// Define the materials
+const mat1 = new THREE.MeshBasicMaterial({ color: 0xff0000 }); // Red
+const mat2 = new THREE.MeshBasicMaterial({ color: 0x00ff33 }); // Green
+const mat3 = new THREE.MeshBasicMaterial({ color: 0x0066ff }); // Blue
+
+// Create the objects
+const cube = new THREE.Mesh(cubeGeometry, mat1);
 scene.add(cube);
+const sphere = new THREE.Mesh(sphereGeometry, mat2);
+sphere.position.x = -2.5;
+scene.add(sphere);
+const sphere2 = new THREE.Mesh(sphereGeometry, mat3);
+sphere2.position.x = 2.5;
+scene.add(sphere2);
 
 // Render Loop
 function animate() {
   requestAnimationFrame(animate); // Setup callback
 
   // Do object calculations
-  cube.rotation.x += 0.02;
-  cube.rotation.y += 0.02;
+  cube.rotation.x += 0.04;
+  cube.rotation.y += 0.03;
+  sphere.rotation.y += 0.03;
+  sphere2.rotation.x += 0.02;
 
   // Render the scene
   renderer.render(scene, camera);

482a7d3357340ab78ecd729aab8fcaf8f39cdf7f
Siddhartha Pratap Singh <sidps1112@gmail.com>
2024-06-05 14:11:17 +0530

    Add primitive, fullscreen and helper widgets

--- src/app.js
+++ src/app.js
@@ -1,18 +1,22 @@
 //! APP
 //-----------------------------------------------
 // Import libraries
+import "../styles.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import gsap from "gsap";
 
 // Define the Canvas
 const canvasSize = {
-  width: 800,
-  height: 600,
+  width: window.innerWidth,
+  height: window.innerHeight,
 };
+window.addEventListener("resize", resizeRenderer);
 const canvas = document.getElementById("sceneCanvas");
 canvas.width = canvasSize.width;
 canvas.height = canvasSize.height;
+
+window.addEventListener("dblclick", processDoubleClick);
 
 // // Define Camera Settings
 // var pitch = 0; // Angle with X axis
@@ -91,13 +95,43 @@ canvas.height = canvasSize.height;
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
 });
-renderer.setSize(canvas.width, canvas.height);
+renderer.setSize(canvasSize.width, canvasSize.height);
+renderer.setPixelRatio(Math.min(2, window.devicePixelRatio));
+
+function resizeRenderer() {
+  canvasSize.width = window.innerWidth;
+  canvasSize.height = window.innerHeight;
+
+  renderer.setSize(canvasSize.width, canvasSize.height);
+  renderer.setPixelRatio(Math.min(2, window.devicePixelRatio));
+
+  camera.aspect = canvasSize.width / canvasSize.height;
+  camera.updateProjectionMatrix();
+}
+
+function processDoubleClick() {
+  const fullscreenElement =
+    document.fullscreenElement || document.webkitFullscreenElement;
+  if (!fullscreenElement) {
+    if (canvas.requestFullscreen) {
+      canvas.requestFullscreen();
+    } else if (canvas.webkitRequestFullscreen) {
+      canvas.webkitRequestFullscreen();
+    }
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    }
+  }
+}
 
 // Create the Scene
 const scene = new THREE.Scene();
 
 // Define the Camera
-const aspectRatio = canvas.width / canvas.height;
+const aspectRatio = canvasSize.width / canvasSize.height;
 const camera = new THREE.PerspectiveCamera(60, aspectRatio, 0.1, 1000);
 var camDistance = 5;
 camera.position.set(0, 0, camDistance);
@@ -119,18 +153,38 @@ scene.add(camOrtho);
 const ambientLight = new THREE.AmbientLight(0x3a3a3a);
 scene.add(ambientLight);
 const pointLight = new THREE.PointLight(0xffffff, 2, 100);
-pointLight.position.set(0, 0, 0);
+pointLight.position.set(1, 1, 1);
 scene.add(pointLight);
 
 // Define basic geometry
 const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
-const sphereGeometry = new THREE.SphereGeometry(1, 12, 12);
+const sphereGeometry = new THREE.SphereGeometry(1, 18, 18);
 const torusGeometry = new THREE.TorusGeometry(0.8, 0.2, 18, 18);
 
+//-------------------------------------
+const positionsArray = new Float32Array([0, 0, 0, 0, 1, 0, 1, 0, 0]);
+const positionAttribute = new THREE.BufferAttribute(positionsArray, 3);
+const triGeometry = new THREE.BufferGeometry();
+triGeometry.setAttribute("position", positionAttribute);
+
+const triCount = 100;
+const posArray = new Float32Array(triCount * 3 * 3);
+for (var i = 0; i < triCount * 3 * 3; i++) {
+  posArray[i] = Math.random() - 0.5;
+}
+const posAttribute = new THREE.BufferAttribute(posArray, 3);
+const ranGeometry = new THREE.BufferGeometry();
+ranGeometry.setAttribute("position", posAttribute);
+//-------------------------------------
+
 // Define the materials
-const mat1 = new THREE.MeshBasicMaterial({ color: 0xff0000 }); // Red
-const mat2 = new THREE.MeshStandardMaterial({ color: 0x00ff33 }); // Green Lit
+const mat1 = new THREE.MeshBasicMaterial({ color: 0xdd000f }); // Red
+const mat2 = new THREE.MeshStandardMaterial({
+  color: 0x00ff44,
+  roughness: 0.3,
+}); // Green Lit
 const mat3 = new THREE.MeshBasicMaterial({ color: 0x0066ff, wireframe: true }); // Blue Wireframe
+const mat4 = new THREE.MeshBasicMaterial({ color: 0x440088, wireframe: true }); // Blue Wireframe
 
 // Create the scene objects
 const group = new THREE.Group();
@@ -150,9 +204,20 @@ group.add(sphere);
 const torus = new THREE.Mesh(torusGeometry, mat3);
 group.add(torus);
 
+const tri = new THREE.Mesh(triGeometry, mat3);
+group.add(tri);
+
+const ran = new THREE.Mesh(ranGeometry, mat4);
+ran.position.y = -1;
+group.add(ran);
+
 // Add other utilities
 const axesHelper = new THREE.AxesHelper(5);
 scene.add(axesHelper);
+const gridHelper = new THREE.GridHelper(15, 15);
+scene.add(gridHelper);
+const lightHelper = new THREE.PointLightHelper(pointLight, 0.1, 0xffffff);
+scene.add(lightHelper);
 
 // Define some parameters
 const clock = new THREE.Clock();
@@ -161,7 +226,9 @@ const rpm = 4;
 
 // Define controls
 const controls = new OrbitControls(camera, canvas);
+// controls.enabled = false;
 controls.enableDamping = true;
+controls.enablePan = false;
 controls.target.y = group.position.y;
 controls.update();
 
